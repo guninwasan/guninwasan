1353a1ef6b14b760e44413e32f55ab785f6e86fe
Gunin Wasan <98042082+guninwasan@users.noreply.github.com>
2022-05-09 23:38:51 +0530

    Added .js file for the portfolio website

---
+++ script.js
@@ -0,0 +1,8 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.About', { duration: 2000 });
+ScrollReveal().reveal('.Education', { duration: 2000 });
+ScrollReveal().reveal('.Skills', { duration: 2000 });
+ScrollReveal().reveal('.container', { duration: 4000 });
+ScrollReveal().reveal('.Projects', { duration: 2000 });
+ScrollReveal().reveal('.Connect', { duration: 2000 });
+document.getElementById("container");
